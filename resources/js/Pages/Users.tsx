006066a7ea96ec1918e8a073153640d2aec60e7c
Tayanne <tayh.snovaes@gmail.com>
2023-07-26 16:34:22 -0300

    feat: improve table layout with flowbite

--- resources/js/Pages/Users.tsx
+++ resources/js/Pages/Users.tsx
@@ -38,29 +38,44 @@ export default function Users({ auth }: PageProps) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="overflow-hidden shadow-sm sm:rounded-lg bg-white">
-                        <table className="table-auto w-full py-8">
-                            <thead>
-                                <tr>
-                                    <th>Nome</th>
-                                    <th>Email</th>
-                                    <th>Criado em</th>
-                                </tr>
-                            </thead>
-                            <tbody className="">
-                                {data?.map((user) => (
-                                    <tr key={user.id}>
-                                        <td>{user.name}</td>
-                                        <td>{user.email}</td>
-                                        <td>
-                                            {format(
-                                                new Date(user.created_at),
-                                                "dd/MM/yyyy"
-                                            )}
-                                        </td>
+                        <div className="relative overflow-x-auto">
+                            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+                                            Nome
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Email
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Criado em
+                                        </th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    {data?.map((user) => (
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <th
+                                                scope="row"
+                                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                            >
+                                                {user.name}
+                                            </th>
+                                            <td className="px-6 py-4">
+                                                {user.email}
+                                            </td>
+                                            <td className="px-6 py-4">
+                                                {format(
+                                                    new Date(user.created_at),
+                                                    "dd/MM/yyy"
+                                                )}
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
